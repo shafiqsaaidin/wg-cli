1c212f2444ad657243c3f51ea6fe2841cff8bda6
mushagh <mushagh@gmail.com>
2021-03-23 18:00:28 +0800

    add new function

--- wg-cli.sh
+++ wg-cli.sh
@@ -2,6 +2,94 @@
 
 # Variables
 WG_INT="wg0"
+BOT_API=
+GROUP_ID=
+
+new_client_setup () {
+	# Given a list of the assigned internal IPv4 addresses, obtain the lowest still
+	# available octet. Important to start looking at 2, because 1 is our gateway.
+	octet=2
+	while grep AllowedIPs /etc/wireguard/${WG_INT}.conf | cut -d "." -f 4 | cut -d "/" -f 1 | grep -q "$octet"; do
+		(( octet++ ))
+	done
+	# Don't break the WireGuard configuration in case the address space is full
+	if [[ "$octet" -eq 255 ]]; then
+		echo "253 clients are already configured. The WireGuard internal subnet is full!"
+		exit
+	fi
+	key=$(wg genkey)
+	psk=$(wg genpsk)
+	# Configure client in the server
+	cat << EOF >> /etc/wireguard/${WG_INT}.conf
+# BEGIN_PEER $client
+[Peer]
+PublicKey = $(wg pubkey <<< $key)
+PresharedKey = $psk
+AllowedIPs = 10.7.0.$octet/32
+# END_PEER $client
+EOF
+	# Create client configuration
+	cat << EOF > ~/"$client".conf
+[Interface]
+Address = 10.7.0.$octet/24
+DNS = 8.8.8.8, 8.8.4.4
+PrivateKey = $key
+
+[Peer]
+PublicKey = $(grep PrivateKey /etc/wireguard/wg0.conf | cut -d " " -f 3 | wg pubkey)
+PresharedKey = $psk
+AllowedIPs = 0.0.0.0/0
+Endpoint = $(grep '^# ENDPOINT' /etc/wireguard/wg0.conf | cut -d " " -f 3):$(grep ListenPort /etc/wireguard/wg0.conf | cut -d " " -f 3)
+PersistentKeepalive = 25
+EOF
+}
+
+check_client () {
+	sed -n -e '/# BEGIN_PEER tgiskandar/ ,/# END_PEER tgiskandar/p' /etc/wireguard/wg0.conf
+}
+
+delete_client () {
+	number_of_clients=$(grep -c '^# BEGIN_PEER' /etc/wireguard/${WG_INT}.conf)
+	if [[ "$number_of_clients" = 0 ]]; then
+		echo
+		echo "There are no existing clients!"
+		exit
+	fi
+	echo
+	echo "Select the client to remove:"
+	grep '^# BEGIN_PEER' /etc/wireguard/${WG_INT}.conf | cut -d ' ' -f 3 | nl -s ') '
+	read -p "Client: " client_number
+	until [[ "$client_number" =~ ^[0-9]+$ && "$client_number" -le "$number_of_clients" ]]; do
+		echo "$client_number: invalid selection."
+		read -p "Client: " client_number
+	done
+	client=$(grep '^# BEGIN_PEER' /etc/wireguard/${WG_INT}.conf | cut -d ' ' -f 3 | sed -n "$client_number"p)
+	echo
+	read -p "Confirm $client removal? [y/N]: " remove
+	until [[ "$remove" =~ ^[yYnN]*$ ]]; do
+		echo "$remove: invalid selection."
+		read -p "Confirm $client removal? [y/N]: " remove
+	done
+	if [[ "$remove" =~ ^[yY]$ ]]; then
+		# The following is the right way to avoid disrupting other active connections:
+		# Remove from the live interface
+		wg set ${WG_INT} peer "$(sed -n "/^# BEGIN_PEER $client$/,\$p" /etc/wireguard/${WG_INT}.conf | grep -m 1 PublicKey | cut -d " " -f 3)" remove
+		# Remove from the configuration file
+		sed -i "/^# BEGIN_PEER $client/,/^# END_PEER $client/d" /etc/wireguard/${WG_INT}.conf
+		echo
+		echo "$client removed!"
+	else
+		echo
+		echo "$client removal aborted!"
+	fi
+}
+
+sync_config () {
+	echo "Reloading wireguard config..."
+	wg syncconf ${WG_INT} <(wg-quick strip ${WG_INT})
+	echo "Done"
+	exit
+}
 
 echo -e "
 VPNJE WG-CLI
@@ -78,49 +166,6 @@ case "$option" in
 			exit
 		;;
         4)
-                echo
-                echo "Reloading wireguard config..."
-                wg syncconf ${WG_INT} <(wg-quick strip ${WG_INT})
-                echo "Done"
-                exit
+
         ;;
 esac
-
-new_client_setup () {
-	# Given a list of the assigned internal IPv4 addresses, obtain the lowest still
-	# available octet. Important to start looking at 2, because 1 is our gateway.
-	octet=2
-	while grep AllowedIPs /etc/wireguard/${WG_INT}.conf | cut -d "." -f 4 | cut -d "/" -f 1 | grep -q "$octet"; do
-		(( octet++ ))
-	done
-	# Don't break the WireGuard configuration in case the address space is full
-	if [[ "$octet" -eq 255 ]]; then
-		echo "253 clients are already configured. The WireGuard internal subnet is full!"
-		exit
-	fi
-	key=$(wg genkey)
-	psk=$(wg genpsk)
-	# Configure client in the server
-	cat << EOF >> /etc/wireguard/${WG_INT}.conf
-# BEGIN_PEER $client
-[Peer]
-PublicKey = $(wg pubkey <<< $key)
-PresharedKey = $psk
-AllowedIPs = 10.7.0.$octet/32$(grep -q 'fddd:2c4:2c4:2c4::1' /etc/wireguard/wg0.conf && echo ", fddd:2c4:2c4:2c4::$octet/128")
-# END_PEER $client
-EOF
-	# Create client configuration
-	cat << EOF > ~/"$client".conf
-[Interface]
-Address = 10.7.0.$octet/24$(grep -q 'fddd:2c4:2c4:2c4::1' /etc/wireguard/wg0.conf && echo ", fddd:2c4:2c4:2c4::$octet/64")
-DNS = $dns
-PrivateKey = $key
-
-[Peer]
-PublicKey = $(grep PrivateKey /etc/wireguard/wg0.conf | cut -d " " -f 3 | wg pubkey)
-PresharedKey = $psk
-AllowedIPs = 0.0.0.0/0, ::/0
-Endpoint = $(grep '^# ENDPOINT' /etc/wireguard/wg0.conf | cut -d " " -f 3):$(grep ListenPort /etc/wireguard/wg0.conf | cut -d " " -f 3)
-PersistentKeepalive = 25
-EOF
-}
